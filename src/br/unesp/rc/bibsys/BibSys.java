261dd0715ee52f77133c1a70d55040ffbef1e13f
Nathalia Franca <nath.campreguer@gmail.com>
2014-06-17 23:30:22 -0300

    Basic template

---
+++ src/br/unesp/rc/bibsys/BibSys.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.unesp.rc.bibsys;
+
+/**
+ *
+ * @author nathalia
+ */
+public class BibSys {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
